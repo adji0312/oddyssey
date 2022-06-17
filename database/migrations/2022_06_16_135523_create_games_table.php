1dc4639827d8320fa7ad37b00806b2a3c5d3c9b7
adji0312 <adjibudhi77@gmail.com>
2022-06-17 09:28:38 +0700

    update migration

---
+++ database/migrations/2022_06_16_135523_create_games_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('categoryID');
+            $table->foreignId('reviewID');
+            $table->string('title');
+            $table->string('description');
+            $table->string('thumbnail');
+            $table->string('slidesPicture');
+            $table->integer('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}
